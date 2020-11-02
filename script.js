487a0ae24914c522007df1bd459eb4a4d2e3c822
mayameapen123 <63078318+mayameapen123@users.noreply.github.com>
2020-11-02 11:22:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 $(".infograph").addClass("chosen");
+$(".c1").click(function() {
+   
+  window.location.href='https://mayameapen123.github.io/Coexist1.github.io/';
+});
+
+$(".c2").click(function() {
+   
+  window.location.href='https://mayameapen123.github.io/coexist2.github.io/';
+});
+
+$(".c3").click(function() {
+   
+  window.location.href='https://mayameapen123.github.io/coexist3.github.io/';
+});
